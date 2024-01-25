485ac68d97f10b4e5dc2ab0e604913ced7c1f6e9
Sergey Sobol <eskapte@mail.ru>
2024-01-25 23:37:11 +0300

    Поднастроил lazy load

--- js/init-fancybox.js
+++ js/init-fancybox.js
@@ -3,14 +3,14 @@ const slidePhotos = document.querySelectorAll(".slide__photos");
 slidePhotos.forEach(carousel => {
   new Carousel(carousel, {
     'slidesPerPage' : 1,
-    'preload' : '0'
+    'preload' : 0
   });
 })
 
 const ourProductsCarousel = document.querySelector(".our-products__carousel");
 new Carousel(ourProductsCarousel, {
   'slidesPerPage': 1,
-  'preload': 1
+  'preload': 0
 });
 
 Fancybox.bind("[data-fancybox]", {
