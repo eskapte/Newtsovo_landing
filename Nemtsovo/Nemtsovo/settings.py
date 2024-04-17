ed1002ce885e95e66b769b62c588e7371eeb0001
Sergey Sobol <eskapte@mail.ru>
2024-04-17 21:58:03 +0300

    Настраиваем медиа файлы

--- Nemtsovo/Nemtsovo/settings.py
+++ Nemtsovo/Nemtsovo/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = 'static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = 'media/' if IS_PROD else os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
